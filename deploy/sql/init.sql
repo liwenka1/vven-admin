644033ced5ad6b35c5e0eb01afcf308547ef8426
liwenkai <2020583117@qq.com>
2023-12-18 18:03:02 +0800

    fix: docker配置修改

--- deploy/sql/init.sql
+++ deploy/sql/init.sql
@@ -1,9 +1,35 @@
-CREATE USER 'admin'@'%' IDENTIFIED BY '123456';
-GRANT ALL PRIVILEGES ON mydatabase.* TO 'admin'@'%';
-FLUSH PRIVILEGES;
+/*
+ Navicat Premium Data Transfer
 
-USE mydatabase;
-CREATE TABLE User (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-);
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80200
+ Source Host           : localhost:3306
+ Source Schema         : vvenadmin_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 80200
+ File Encoding         : 65001
+
+ Date: 18/12/2023 14:49:04
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for User
+-- ----------------------------
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of User
+-- ----------------------------
+INSERT INTO `User` VALUES (1, 'good');
+
+SET FOREIGN_KEY_CHECKS = 1;
