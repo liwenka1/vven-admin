443020d89856176e992f5bfb5b5e573901c68b05
liwenkai <2020583117@qq.com>
2023-12-21 18:03:35 +0800

    fix: 面包屑和菜单栏

--- packages/web/src/components/layout/header/breadcrumb.tsx
+++ packages/web/src/components/layout/header/breadcrumb.tsx
@@ -1,44 +1,56 @@
-import React from 'react'
-
 import { Breadcrumb } from 'antd'
 import { Link } from 'react-router-dom'
 
-const items = [
+type Paths = typeof paths
+
+const paths = [
+  { name: 'Navigation One', path: '/1', children: [{ name: 'Navigation One', path: '/home' }] },
+  { name: 'Navigation One', path: '/home' }
+] as const
+
+const itemsRender = (paths: Paths) => {
+  const items = []
+  for (const path of paths) {
+    const item = {}
+    item.key = path.path
+    item.title = <Link to={path.path}>{path.name}</Link>
+    items.push(item)
+  }
+  console.log(items)
+  return items
+}
+
+const menuItems = [
   {
-    path: '/home',
-    title: 'home'
+    key: '1',
+    label: <Link to={'/home'}>General</Link>
   },
   {
-    path: 'first',
-    title: 'first',
-    menu: {
-      items: [
-        {
-          path: '/general',
-          title: 'General'
-        },
-        {
-          path: '/layout',
-          title: 'Layout'
-        },
-        {
-          path: '/navigation',
-          title: 'Navigation'
-        }
-      ]
-    }
+    key: '2',
+    label: <Link to={'/user'}>Layout</Link>
   },
   {
-    path: 'second',
-    title: 'second'
+    key: '3',
+    label: <Link to={'/about'}>Navigation</Link>
   }
 ]
 
-const itemRender = (item, params, items, paths) => {
-  const last = items.indexOf(item) === items.length - 1
-  return last ? <span>{item.title}</span> : <Link to={paths.join('/')}>{item.title}</Link>
-}
+const items = [
+  {
+    title: 'Ant Design'
+  },
+  {
+    title: <Link to={'/menu'}>Component</Link>
+  },
+  {
+    title: <Link to={'/menu'}>General</Link>,
+    menu: { items: menuItems }
+  },
+  {
+    title: 'Button'
+  }
+]
 
-const HeaderBreadcrumb: React.FC = () => <Breadcrumb itemRender={itemRender} items={items} />
+const HeaderBreadcrumb: React.FC = () => <Breadcrumb items={itemsRender(paths)} />
 
 export default HeaderBreadcrumb
--- packages/web/src/components/layout/sider/menu.tsx
+++ packages/web/src/components/layout/sider/menu.tsx
@@ -21,26 +21,18 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-  getItem('Navigation One', '/home', <MailOutlined />),
-  getItem('Navigation Two', '/about', <CalendarOutlined />),
-  getItem('Navigation Two', '/user', <AppstoreOutlined />, [
-    getItem('Option 3', '3'),
-    getItem('Option 4', '4'),
-    getItem('Submenu', 'sub1-2', null, [getItem('Option 5', '5'), getItem('Option 6', '6')])
-  ]),
-  getItem('Navigation Three', '/menu', <SettingOutlined />, [
-    getItem('Option 7', '7'),
-    getItem('Option 8', '8'),
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10')
-  ])
+  getItem('Navigation One', '/1', <MailOutlined />, [getItem('Option home', '/home')]),
+  getItem('Navigation Two', '/2', <CalendarOutlined />, [getItem('Option about', '/about')]),
+  getItem('Navigation Two', '/3', <AppstoreOutlined />, [getItem('Option user', '/user')]),
+  getItem('Navigation Three', '/4', <SettingOutlined />, [getItem('Option menu', '/menu')])
 ]
 
 const SiderMenu = () => {
   const navigate = useNavigate()
   const { openKeys, setOpenKeys, selectedKeys, setSelectedKeys } = useGlobalStore()
 
-  const handleMenuClick: MenuProps['onClick'] = ({ key }) => {
+  const handleMenuClick: MenuProps['onClick'] = ({ key, keyPath }) => {
+    setOpenKeys(keyPath)
     setSelectedKeys([key])
     navigate(key)
   }
