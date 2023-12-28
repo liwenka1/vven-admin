e3bf41662456734c5b2f105589202941d5aa3af0
liwenkai <2020583117@qq.com>
2023-12-28 18:12:26 +0800

    fix: 测试前后端接口联调

--- packages/web/src/api/menu/index.ts
+++ packages/web/src/api/menu/index.ts
@@ -2,6 +2,6 @@ import request from '@/request'
 
 export const menuApi = {
   get: () => {
-    return request.get('/api/menu/search')
+    return request.get<Promise<unknown>>('/api/menu/search')
   }
 }
--- packages/web/src/pages/home/index.tsx
+++ packages/web/src/pages/home/index.tsx
@@ -1,8 +1,11 @@
 import { menuApi } from '@/api/menu'
 
 const Home = () => {
-  const res = menuApi.get()
-  console.log(res)
+  const api = async () => {
+    const res = await menuApi.get()
+    console.log(res)
+  }
+  api()
 
   return <div>这里是HOME</div>
 }
